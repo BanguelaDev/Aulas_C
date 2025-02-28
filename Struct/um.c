8b378eaf75d1a3bdeb95af7120c0f3274843de21
Banguelinha <jgomdev@gmail.com>
2025-02-27 22:08:46 -0300

    Create um.c

---
+++ Struct/um.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student {
+    char name[50];
+    int age;
+    double grade;
+};
+
+int main(){
+    
+    struct student s1;
+    s1.name = "Cristian";
+
+
+}
