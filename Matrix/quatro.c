9247e776cabdde64ab6e5ec41ca50a3d725bb9f3
Banguelinha <jgomdev@gmail.com>
2025-02-28 21:17:37 -0300

    .
    .

--- Matrix/quatro.c
+++ Matrix/quatro.c
@@ -11,6 +11,7 @@ int main(){
         {13, 14, 15, 16}
     };
 
+    
     int total = 0;
 
     for (int i = 0; i < SIZE; i++) {
--- Struct/um.c
+++ Struct/um.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include <string.h>
 
-struct student {
+struct student{
     char name[50];
     int age;
     double grade;
 };
 
+void printStudent(struct student s){
+    printf("Nome: %s, Idade: %d, Nota: %.10g\n", s.name, s.age, s.grade);
+}
+
 int main(){
-    
-    struct student s1;
-    s1.name = "Cristian";
+    struct student s1 = {"Joao", 20, 8.5};
+    struct student s2 = {"Maria", 19, 9.0};
+    struct student s3 = {"Pedro", 21, 7.5};
 
+    printStudent(s1);
+    printStudent(s2);
+    printStudent(s3);
 
+    return 0;
 }
